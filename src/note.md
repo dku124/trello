28dee796896ea45aa7d768bfd958b7b7d82fa361
dku124 <duongkhanhuyen124@gmail.com>
2024-02-23 17:28:26 +0700

    dndpart1

--- src/note.md
+++ src/note.md
@@ -2,3 +2,8 @@ rfce
 
  - import mock data into Boards (_id.jsx)
  - prop 'board' -> Boardbar, BoardContent ->ListColumn-> Column -> listcard -> card
+ - sort BoardContent, Column
+
+ <!-- dndkit -->
+ - Boxcontent, listcolumn, column
+ 30::18:04
--- src/pages/Boards/BoardContent/ListColumn/ListColumn.jsx
+++ src/pages/Boards/BoardContent/ListColumn/ListColumn.jsx
@@ -1,17 +1,20 @@
 import { Box, Button } from "@mui/material"
 import Column from "./Column/Column"
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
+import { SortableContext, horizontalListSortingStrategy } from "@dnd-kit/sortable"
 
 
 function ListColumn({ columns }) {
 
   return (
-    <Box sx={{ bgcolor:'inherit', width:'100%', height:'100%', display:'flex', gap:2, overflowY:'hidden', overflowX: 'auto', p:1, '&::-webkit-scrollbar-track': { m:2 } }}>
-      {columns?.map( column => (
-        <Column key={ column._id } column={column}/>
-      ))}
-      <Button startIcon={<NoteAddIcon />} sx={{ color: '#fff', borderRadius:'6px', bgcolor:'#ffffff3d', mx: 2, height:'fit-content' }}>Add new column</Button>
-    </Box>
+    <SortableContext items={columns} strategy={horizontalListSortingStrategy}>
+      <Box sx={{ bgcolor:'inherit', width:'100%', height:'100%', display:'flex', gap:2, overflowY:'hidden', overflowX: 'auto', p:1, '&::-webkit-scrollbar-track': { m:2 } }}>
+        {columns?.map( column => (
+          <Column key={ column._id } column={column}/>
+        ))}
+        <Button startIcon={<NoteAddIcon />} sx={{ color: '#fff', borderRadius:'6px', bgcolor:'#ffffff3d', mx: 2, height:'fit-content' }}>Add new column</Button>
+      </Box>
+    </SortableContext>
   )
 }
 
